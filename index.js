c7bd00297a751b33e50422d4f4a9a3240ef92cf5
Clara <c_c_456@hotmail.com>
2017-11-09 13:03:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,9 @@ function distanceTravelledInFeet(origin, destination){
 function calculatesFarePrice(origin, destination){
  const distance = distanceTravelledInFeet(origin, destination);
 
-  
+  if(distance<=400){
+    return 0;
+  } else if (distance > 400 && distance <2000){
+    return 2;
+  }
 };
