680ed8333e3a324bbc5f486e7b0bd8917ccfb3f6
Clara <c_c_456@hotmail.com>
2017-11-09 12:54:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(startingBlock){
   if(startingBlock) > 42){
-    return startingBlock) - 42;
+    return startingBlock - 42;
   } else{
-    return 42 - startingBlock);
+    return 42 - startingBlock;
   }
 };
 
