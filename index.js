dbd15463b935777cc46ae57eb7136e5c3bd5f8de
Clara <c_c_456@hotmail.com>
2017-11-09 12:55:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(startingBlock){
-  if(startingBlock) > 42){
-    return startingBlock - 42;
+function distanceFromHqInBlocks(origin){
+  if(origin > 42){
+    return origin - 42;
   } else{
-    return 42 - startingBlock;
+    return 42 - origin;
   }
 };
 
 
+function distanceFromHqInFeet (origin) {
+    return distanceFromHqInBlocks(origin) * 264;
+};
 
+function distanceTravelledInFeet(origin, destination){
+  if(origin < destination){
+    return (destination - origin) * 264;
+  } else{
+    return (origin - destination) * 264;
+  }
+};
+
+function calculatesFarePrice(){
+
+};
