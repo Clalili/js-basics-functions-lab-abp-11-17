ec28a8f93312311e1f90c4ad1f147435c71b6d3a
Clara <c_c_456@hotmail.com>
2017-11-09 12:51:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function distanceFromHqInFeet (numBlocks) {
     return distanceFromHqInBlocks(numBlocks) * 264;
 };
 
-function distanceTravelledInFeet(numBlocks){
-  
-}
+function distanceTravelledInFeet(origin, destination){
+  if(origin < destination){
+    return (destination - origin) * 264;
+  } else{
+    return (origin - destination) * 264;
+  }
+};
