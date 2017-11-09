d684f808f947f9d9daf03c1fe83aa43409fda492
Clara <c_c_456@hotmail.com>
2017-11-09 12:43:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Code your solution in this file!
-let blocks = 43;
 
-function distanceFromHqInBlocks(blocks){
-
+function distanceFromHqInBlocks(numBlocks){
+  if(numblocks > 42){
+    return numBlocks - 42;
+  }
+  else{
+    return 42 - numBlocks;
+  }
 };
 
 function distanceFromHqInFeet(){
